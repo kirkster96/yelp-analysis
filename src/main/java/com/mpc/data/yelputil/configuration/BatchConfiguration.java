a3f4fc414b068da10fa5e25d54f10709ef12bee2
Cameron Kirk <kirkster96@gmail.com>
2024-05-11 13:08:38 -0700

    Add Spring Batch dependencies and configuration
    This commit includes the addition of Spring Batch and Spring Batch Test dependencies to the pom.xml file. A new BatchConfiguration class was also created and now enables scheduling within the project.

---
+++ src/main/java/com/mpc/data/yelputil/configuration/BatchConfiguration.java
@@ -0,0 +1,10 @@
+package com.mpc.data.yelputil.configuration;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class BatchConfiguration {
+
+}

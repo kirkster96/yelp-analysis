55aa27da26c55852cd39b7a1f7bd64597f3f716f
Cameron Kirk <kirkster96@gmail.com>
2024-05-19 09:42:54 -0700

    Migrate Business model to Kotlin and update project dependencies
    The Business model has been migrated from Java to Kotlin to leverage Kotlin's concise syntax and other benefits. Additionally, project dependencies in the pom.xml file have been updated with the inclusion of dependencies for Kotlin, Spring Boot Starter Security, and related testing libraries. These changes will improve the efficiency and maintainability of the project.

--- src/main/java/com/mpc/data/yelputil/model/Business.kt
+++ src/main/java/com/mpc/data/yelputil/model/Business.kt
@@ -1,24 +1,23 @@
-package com.mpc.data.yelputil.model;
+package com.mpc.data.yelputil.model
 
-import java.util.Map;
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("businesses")
-public record Business(
-		@Id String business_id,
-		String name,
-		String address,
-		String city,
-		String state,
-		String postal_code,
-		Double latitude,
-		Double longitude,
-		Float stars,
-		Integer review_count,
-		Integer is_open,
-		Map<String, String> attributes,
-		String categories,
-		Map<String, String> hours){
-}
+data class Business(
+    @Id val businessId: String,
+    val name: String,
+    val address: String,
+    val city: String,
+    val state: String,
+    val postalCode: String,
+    val latitude: Double,
+    val longitude: Double,
+    val stars: Float,
+    val reviewCount: Int,
+    val isOpen: Int,
+    val attributes: Map<String, String>,
+    val categories: String,
+    val hours: Map<String, String>
+)
--- src/main/java/com/mpc/data/yelputil/repository/BusinessRepository.java
+++ src/main/java/com/mpc/data/yelputil/repository/BusinessRepository.java
@@ -1,7 +1,5 @@
 package com.mpc.data.yelputil.repository;
 
-import java.util.List;
-
 import com.mpc.data.yelputil.model.Business;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
